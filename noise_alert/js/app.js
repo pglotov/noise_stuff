7ee535780bf755e8d6f398c5affc5a30f732d9df
Petr Glotov <pglotov@yahoo.com>
2014-11-11 07:22:36 -0800

    no

--- noise_alert/js/app.js
+++ noise_alert/js/app.js
@@ -34,74 +34,71 @@
         restrict: 'E',
         scope: {
           threshold: '=',
-          noiseData: '=',
-          controller: [
-            '$scope', '$window', function($scope, $window) {
-              var e, noiseData;
-              noiseData = $scope.noiseData;
-              noiseData.instant = 0.0;
-              noiseData.noiseProgress = 0;
-              noiseData.cumulativeVolume = 0;
-              noiseData.topNoises = [];
-              try {
-                $window.AudioContext = $window.AudioContext || $window.webkitAudioContext;
-                $scope.context = new AudioContext();
-              } catch (_error) {
-                e = _error;
-                alert('Web Audio API not supported.');
-              }
-              $scope.script = $scope.context.createScriptProcessor(2048, 1, 1);
-              $scope.script.onaudioprocess = function(event) {
-                var i, input, sum, _i, _ref;
-                input = event.inputBuffer.getChannelData(0);
-                sum = 0.0;
-                for (i = _i = 0, _ref = input.length; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
-                  sum += input[i] * input[i];
-                }
-                noiseData.instant = Math.sqrt(sum / input.length);
-                if (noiseData.instant > $scope.threshold) {
-                  noiseData.noiseProgress += input.length / 2048;
-                  return noiseData.cumulativeVolume += that.instant * input.length / 2048;
-                } else {
-                  if (noiseData.noiseProgress >= 10000) {
-                    noiseData.topNoises.push_back({
-                      cumulativeVolume: noiseData.cumulativeVolume,
-                      timestamp: new Date()
-                    });
-                    noiseData.topNoises.reverse(function(a, b) {
-                      return b.cumulativeVolume - a.cumulativeVolume;
-                    });
-                    if (noiseData.topNoises.length > 3) {
-                      noiseData.topNoises.pop();
-                    }
-                  }
-                  noiseData.noiseProgress = 0;
-                  return noiseData.cumulativeVolume = 0;
-                }
-              };
-              $scope.connectToSource = function(stream) {
-                console.log('SoundMeter connecting');
-                $scope.mic = $scope.context.createMediaStreamSource(stream);
-                $scope.mic.connect(this.script);
-                return $scope.script.connect($scope.context.destination);
-              };
-              $scope.stop = function() {
-                $scope.mic.disconnect();
-                return $scope.script.disconnect();
-              };
-              $scope.constraints = {
-                audio: true,
-                video: false
-              };
-              navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-              return navigator.getUserMedia(constraints, (function(stream) {
-                return $scope.connectToSource(stream);
-              }), (function(error) {
-                return console.log('navigator.getUserMedia error: ', error);
-              }));
+          noiseData: '='
+        },
+        controller: [
+          '$scope', '$window', function($scope, $window) {
+            var e, i, input, noiseData, sum, _i, _ref;
+            noiseData = $scope.noiseData;
+            noiseData.instant = 0.0;
+            noiseData.noiseProgress = 0;
+            noiseData.cumulativeVolume = 0;
+            noiseData.topNoises = [];
+            try {
+              $window.AudioContext = $window.AudioContext || $window.webkitAudioContext;
+              $scope.context = new AudioContext();
+            } catch (_error) {
+              e = _error;
+              alert('Web Audio API not supported.');
             }
-          ]
-        }
+            $scope.script = $scope.context.createScriptProcessor(2048, 1, 1);
+            $scope.script.onaudioprocess = function(event) {};
+            input = event.inputBuffer.getChannelData(0);
+            sum = 0.0;
+            for (i = _i = 0, _ref = input.length; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+              sum += input[i] * input[i];
+            }
+            noiseData.instant = Math.sqrt(sum / input.length);
+            if (noiseData.instant > $scope.threshold) {
+              noiseData.noiseProgress += input.length / 2048;
+              noiseData.cumulativeVolume += that.instant * input.length / 2048;
+            } else {
+              if (noiseData.noiseProgress >= 10000) {
+                noiseData.topNoises.push_back({
+                  cumulativeVolume: noiseData.cumulativeVolume,
+                  timestamp: new Date()
+                });
+                noiseData.topNoises.reverse(function(a, b) {
+                  return b.cumulativeVolume - a.cumulativeVolume;
+                });
+                if (noiseData.topNoises.length > 3) {
+                  noiseData.topNoises.pop();
+                }
+                noiseData.noiseProgress = 0;
+                noiseData.cumulativeVolume = 0;
+              }
+            }
+            $scope.connectToSource = function(stream) {};
+            console.log('SoundMeter connecting');
+            $scope.mic = $scope.context.createMediaStreamSource(stream);
+            $scope.mic.connect(this.script);
+            $scope.script.connect($scope.context.destination);
+            $scope.stop = function() {
+              $scope.mic.disconnect();
+              return $scope.script.disconnect();
+            };
+            $scope.constraints = {
+              audio: true,
+              video: false
+            };
+            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+            return navigator.getUserMedia(constraints, (function(stream) {
+              return $scope.connectToSource(stream);
+            }), (function(error) {
+              return console.log('navigator.getUserMedia error: ', error);
+            }));
+          }
+        ]
       };
     }
   ]);
--- noise_alert/js/soundMeter.coffee
+++ noise_alert/js/soundMeter.coffee
@@ -5,62 +5,62 @@ app = angular.module 'noiseAlert.app'
     scope:
         threshold: '='
         noiseData: '='
-        controller: ['$scope', '$window', ($scope, $window)->
-            noiseData = $scope.noiseData
-            noiseData.instant = 0.0;
-            noiseData.noiseProgress = 0;
-            noiseData.cumulativeVolume = 0;
-            noiseData.topNoises = [];
+    controller: ['$scope', '$window', ($scope, $window)->
+        noiseData = $scope.noiseData
+        noiseData.instant = 0.0;
+        noiseData.noiseProgress = 0;
+        noiseData.cumulativeVolume = 0;
+        noiseData.topNoises = [];
 
 
-            try
-                $window.AudioContext = $window.AudioContext || $window.webkitAudioContext;
-                $scope.context = new AudioContext();
-            catch e
-                alert('Web Audio API not supported.');
+        try
+            $window.AudioContext = $window.AudioContext || $window.webkitAudioContext;
+            $scope.context = new AudioContext();
+        catch e
+            alert('Web Audio API not supported.');
 
-            $scope.script = $scope.context.createScriptProcessor(2048, 1, 1);
+        $scope.script = $scope.context.createScriptProcessor(2048, 1, 1);
 
-            $scope.script.onaudioprocess = (event)->
-                input = event.inputBuffer.getChannelData(0)
-                sum = 0.0
-                for i in [0..input.length]
-                    sum += input[i] * input[i]
-                noiseData.instant = Math.sqrt(sum / input.length)
+        $scope.script.onaudioprocess = (event)->
+        input = event.inputBuffer.getChannelData(0)
+        sum = 0.0
+        for i in [0..input.length]
+            sum += input[i] * input[i]
+        noiseData.instant = Math.sqrt(sum / input.length)
 
-                if noiseData.instant > $scope.threshold
-                    noiseData.noiseProgress += input.length / 2048;
-                    noiseData.cumulativeVolume += that.instant * input.length / 2048;
-                else
-                    if noiseData.noiseProgress >= 10000
-                        noiseData.topNoises.push_back
-                            cumulativeVolume: noiseData.cumulativeVolume
-                            timestamp: new Date()
-                        noiseData.topNoises.reverse((a,b)-> b.cumulativeVolume - a.cumulativeVolume)
-                        if noiseData.topNoises.length > 3
-                            noiseData.topNoises.pop()
-                    noiseData.noiseProgress = 0
-                    noiseData.cumulativeVolume = 0
+        if noiseData.instant > $scope.threshold
+            noiseData.noiseProgress += input.length / 2048;
+            noiseData.cumulativeVolume += that.instant * input.length / 2048;
+        else
+            if noiseData.noiseProgress >= 10000
+                noiseData.topNoises.push_back
+                    cumulativeVolume: noiseData.cumulativeVolume
+                    timestamp: new Date()
+                noiseData.topNoises.reverse((a,b)-> b.cumulativeVolume - a.cumulativeVolume)
+                if noiseData.topNoises.length > 3
+                    noiseData.topNoises.pop()
+                noiseData.noiseProgress = 0
+                noiseData.cumulativeVolume = 0
 
-            $scope.connectToSource = (stream)->
-                console.log('SoundMeter connecting');
-                $scope.mic = $scope.context.createMediaStreamSource(stream);
-                $scope.mic.connect(this.script);
-                # necessary to make sample run, but should not be.
-                $scope.script.connect($scope.context.destination);
+        $scope.connectToSource = (stream)->
+        console.log('SoundMeter connecting');
+        $scope.mic = $scope.context.createMediaStreamSource(stream);
+        $scope.mic.connect(this.script);
+        # necessary to make sample run, but should not be.
+        $scope.script.connect($scope.context.destination);
 
-            $scope.stop = ()->
-                $scope.mic.disconnect();
-                $scope.script.disconnect();
+        $scope.stop = ()->
+            $scope.mic.disconnect();
+            $scope.script.disconnect();
 
-            $scope.constraints =
-                audio: true
-                video: false
+        $scope.constraints =
+            audio: true
+            video: false
 
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-            navigator.getUserMedia constraints, ((stream)->
-                $scope.connectToSource(stream)),
-                ((error)->console.log('navigator.getUserMedia error: ', error))
-        ]
+        navigator.getUserMedia constraints, ((stream)->
+            $scope.connectToSource(stream)),
+            ((error)->console.log('navigator.getUserMedia error: ', error))
+    ]
 ]
